fba97372a6bb2cbbd056fd06d84d4d5b05cf14a5
tamanna223 <tamannab931@gmail.com>
2025-10-08 18:47:41 +0530

    fix: client bootstrap loads page props for static deploy

--- renderer/entry-client.jsx
+++ renderer/entry-client.jsx
@@ -4,6 +4,20 @@ import Page from '../pages/web-designer/+Page.jsx'
 import './global.css'
 
 const rootEl = document.getElementById('page-view')
-const props = window.__PAGE_PROPS__ || {}
 
-ReactDOM.createRoot(rootEl).render(<Page {...props} />)
+async function bootstrap(){
+  // Load props from client since static deploy doesn't inject SSR props
+  let props = {}
+  try{
+    const mod = await import('../pages/web-designer/+onBeforeRender.js')
+    if(mod && typeof mod.default === 'function'){
+      const { pageContext } = await mod.default()
+      props = (pageContext && pageContext.pageProps) || {}
+    }
+  }catch(err){
+    console.error('Failed to load page props:', err)
+  }
+  ReactDOM.createRoot(rootEl).render(<Page {...props} />)
+}
+
+bootstrap()
